1a266f4d0018e2b447af978b04e6ce49b3fa240a
Nicholas Dickey <nicholas.dickey@hudsonwilde.com>
2023-01-10 16:09:07 +0000

    Clean-up ts.

---
+++ pages/api/v1/newsline/updateAll.ts
@@ -0,0 +1,66 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import NextCors from 'nextjs-cors';
+import { l, chalk, js } from "../../../../lib/common";
+import { getRedisClient } from "../../../../lib/redis"
+import { updateSessionNewsline, updateUserNewsline } from "../../../../lib/db/newsline"
+import { dbLog, dbEnd } from "../../../../lib/db"
+import fetchAll from '../../../../lib/fetchAll';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
+   
+
+    console.log("===*** &&& === inside newsline update all handler",req.body)
+    const body = req.body;
+    let { sessionid, userslug, newsline, switch: switchParam, tag,filters,q }: { sessionid?: string, userslug?: string, newsline: string, switch: 'on' | 'off', tag: string, filters: string[], q?: string } = body;
+
+    const id = userslug || sessionid;
+    let threadid = Math.floor(Math.random() * 100000000)
+    const redis = await getRedisClient({});
+
+    if (!redis)
+        return res.status(500).json({ msg: "Unable to create redis" })
+    if (!id)
+        return res.status(200).json({ success: false, msg: "userslug or sessionid is missing" })
+    try {
+
+        const userNewslineKey = `newsline-${newsline}-${id}`;
+
+        if (userslug) {
+            await updateUserNewsline({ threadid, key: `${newsline}-${userslug}`, tag, switchParam, userslug })
+
+        }
+        else if (sessionid) {
+            await updateSessionNewsline({ threadid, key: `${newsline}-${sessionid}`, tag, switchParam, sessionid })
+        }
+
+        await redis.del(userNewslineKey);  //next fetch would repopulate redis from db.   
+        const publications=await fetchAll({redis,threadid,sessionid,userslug,newsline,filters,q})
+        l(chalk.yellow.bold("================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Result myNewsline:", js(publications)));
+        return res.status(200).json({
+            success: true,
+            publications
+        })
+    }
+    catch (x) {
+        l(chalk.red.bold("Exception in updateAll", x));
+        return res.status(500).json({})
+    }
+    finally {
+        dbEnd(threadid);
+        redis.quit();
+    }
+
+    res.status(200).json({})
+}
