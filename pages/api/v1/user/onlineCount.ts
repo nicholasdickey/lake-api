8cfd8fcc1854fe82183fde5c93accf854519344a
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-02-16 12:16:08 -0500

    Add onlineCount

--- pages/api/v1/user/onlineCount.ts
+++ pages/api/v1/user/onlineCount.ts
@@ -2,13 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors';
 import { l, chalk, js } from "../../../../lib/common";
 import { getRedisClient } from "../../../../lib/redis"
-import { getUser } from "../../../../lib/db/user"
 import { dbLog, dbEnd } from "../../../../lib/db"
 
  const  onlineCount=async (identity:string,redis:any)=> {
-    // let zcount = await redis.zcount('online', '-inf', '+inf');
-    // console.log("onlineCount=", count, identity)
-
     if (identity) {
         if (Array.isArray(identity)) identity = identity[0];
         // console.log("onlineCount2=", count, identity)
@@ -31,21 +27,13 @@ import { dbLog, dbEnd } from "../../../../lib/db"
             redis.zadd(mq, (Date.now() / 1000) | 0, identity);
            
         }
-        redis.publish("drsyncdb", "update");
+
         redis.expire(q, 365 * 24 * 3600);
         //redis.zremrangebyscore(q,0,((Date.now()/1000)|0)-30*24*3600)
         redis.zadd("online", (Date.now() / 1000) | 0, identity);
         redis.expire("online", 365 * 24 * 3600);
 
-        o = drsyncFormat(
-            "redis-zadd",
-            "online||" + ((Date.now() / 1000) | 0),
-            identity,
-            365 * 24 * 3600
-        );
-        //   console.log("%%% %%% %%% DRSYNC ",o);
-        redis.lpush("drsyncdb", o);
-        redis.publish("drsyncdb", "update");
+       
 
         await redis.zremrangebyscore(
             "online",
@@ -54,9 +42,11 @@ import { dbLog, dbEnd } from "../../../../lib/db"
         );
         let count = await redis.zcount("online", "-inf", "+inf");
         let dayCount = await redis.zcount(q, "-inf", "+inf");
-        return `{"success":true,"count":${count},"daycount":${dayCount}}`;
+        return {success:true,
+            count,
+            daycount:dayCount};
     } else {
-        return resolve(`{"success":false}`);
+        return {"success":false};
     }
 }
 
@@ -74,10 +64,10 @@ export default async function handler(
         origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
-    l(chalk.yellow("fetch user"));
+    l(chalk.yellow("onlineCount"));
     let { userslug,sessionid } = req.query;
     const id=userslug||sessionid||'';
-    let threadid = Math.floor(Math.random() * 100000000)
+   // let threadid = Math.floor(Math.random() * 100000000)
     const redis = await getRedisClient({});
     if (!redis)
         return res.status(500).json({msg:"Unable to create redis"})
@@ -85,26 +75,9 @@ export default async function handler(
         return res.status(200).json({user:{}})
    
     try {
-
-        const userKey = `user-${userslug}`;
-        console.log("redis.get",userKey)
-        let user = await redis.get(userKey);
-        console.log("redis.get",userKey,user)
-        let jsonUser;
-        if (!user) {
-            l(chalk.red("USER FETCH NO user, calling db"))
-            jsonUser = await getUser({ threadid, slug:userslug as string })
-            user=JSON.stringify(jsonUser)
-            redis.setex(userKey, 365 * 24 * 3600, user);
-        }
-        else {
-            jsonUser=JSON.parse(user);
-        }
-       
-        if (!jsonUser)
-            return res.status(500).json({msg:"Unable to parse user"});
- 
-        res.status(200).json({success:true,user:jsonUser})
+        const ret=await onlineCount(id as string||'',redis);
+        l(chalk.yellow("onlineCount",js(ret)));
+        res.status(200).json(ret);
     }
 
     catch (x) {
@@ -114,7 +87,7 @@ export default async function handler(
     }
     finally{
         redis.quit();
-        dbEnd(threadid);
+      //  dbEnd(threadid);
     }
 
 }
