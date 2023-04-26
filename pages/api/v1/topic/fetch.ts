c46405d793bd605c36e68942a3f4fcff71c0d711
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-04-25 20:14:52 -0400

    Brought back redis cache of topic

--- pages/api/v1/topic/fetch.ts
+++ pages/api/v1/topic/fetch.ts
@@ -7,7 +7,7 @@ import { dbLog, dbEnd } from "../../../../lib/db";
 import { getQwiket } from "../../../../lib/db/qwiket";
 import { processBody } from "../../../../lib/process-body";
 import { Qwiket } from "../../../../lib/types/qwiket";
-import { verifyAck } from "../../../../lib/db/user" 
+import { verifyAck } from "../../../../lib/db/user"
 interface Query {
     slug?: string,
     withBody: [0, 1],
@@ -27,16 +27,16 @@ export default async function handler(
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
-    let { slug='', withBody, userslug, sessionid, tag, ack }: Query = req.query as unknown as Query;
+    let { slug = '', withBody, userslug, sessionid, tag, ack }: Query = req.query as unknown as Query;
     let threadid = Math.floor(Math.random() * 100000000)
     const redis = await getRedisClient({});
     if (!redis)
         return res.status(500).json({ msg: "Unable to connect to redis" });
     try {
         let json: any;
-     let txid: string = '';
+        let txid: string = '';
         try {
-            if(slug!=''){
+            if (slug != '') {
                 const key = `txid-${slug}`;
                 txid = await redis.get(key) || '';
             }
@@ -45,18 +45,22 @@ export default async function handler(
                 const keyTxid = `tids-cat-published-${tag}`;
                 const range = await redis.zrevrange(keyTxid, 0, 0);
                 txid = range[0];
-            }     
+            }
         }
         catch (x) {
             l(chalk.red.bold(x))
         }
-        if (true) {
+        const key = `ntjson-${withBody + '-'}${txid}`;
+        const jsonRaw = await redis.get(key);
+        if (jsonRaw)
+            json = JSON.parse(jsonRaw)
+        if (!json) {
             // get from db
             json = await getQwiket({ threadid, slug, withBody, tag })
             if (json && withBody) {
-                l("json1:",js(json.body))
+                l("json1:", js(json.body))
                 json.body = processBody(json);
-                l("json2:",js(json.body))
+                l("json2:", js(json.body))
                 const key = `ntjson-${withBody + '-'}${txid}`;
                 const jsonRaw = JSON.stringify(json);
                 try {
@@ -87,7 +91,7 @@ export default async function handler(
             image: item.image,
             tag: item.cat,
             body: item.body,
-            headless:item.headless
+            headless: item.headless
         }
         /**
          * Check the user acceptances
