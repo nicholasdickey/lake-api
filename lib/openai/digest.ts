2268414cbcd72127d287a9bbd0a6306505a77c06
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-07-12 11:21:32 -0400

    Change ads to sponsor in digest

--- lib/openai/digest.ts
+++ lib/openai/digest.ts
@@ -467,10 +467,10 @@ export default async function handler(
     let html = ''
     let json: any = {}
     if (ads.length > 0) {
-        json.ads = { items: [] };
+        json.sponsor = { items: [] };
         html += `<div class='digest-ads'><div class='digest-hashtag'>#sponsor</div>\n`
         ads.forEach(a => {
-            json.ads.items.push({ title: a.title, url: a.url, text: a.text, publication: a.publication, image: a.image });
+            json.sponsor.items.push({ title: a.title, url: a.url, text: a.text, publication: a.publication, image: a.image });
             html += `<span class='digest-title'><a href='${a.url}'>${a.publication}: ${a.title}</a></span><p class='digest-text'>${a.text.split('#sponsor')[0]}</p>\n`;
         })
         html += `</div>`;
