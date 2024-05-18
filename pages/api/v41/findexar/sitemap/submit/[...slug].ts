b3b1bdb8869adf77fd95817a75e0694c3dd4d1ad
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-05-17 21:48:49 -0400

    Add no league cache refresh.

--- pages/api/v41/findexar/sitemap/submit/[...slug].ts
+++ pages/api/v41/findexar/sitemap/submit/[...slug].ts
@@ -43,7 +43,7 @@ export default async function handler(
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
     let threadid = Math.floor(Math.random() * 100000000);
-  
+
     try {
         const params = req.query?.slug as string[];
         let [slug] = params || ['',];
@@ -52,37 +52,33 @@ export default async function handler(
         const leagues = await getSlugLeagues({ threadid, slug });
         const date = getDayInPast(7);
         domains.forEach(async (domain) => {
-            console.log("process domain",domain,leagues,slug)
+            console.log("process domain", domain, leagues, slug)
             leagues.forEach(async (league: string) => {
-                console.log("process league",domain,leagues,slug)
+                console.log("process league", domain, leagues, slug)
                 const url = `https://${domain}/${league}?story=${slug}`;
                 const sitemapName = `${date}`
-                console.log("sitemap url", url, sitemapName,domain,league);
+                console.log("sitemap url", url, sitemapName, domain, league);
                 await indexUrl(url);
-                await submitCurrentSitemap(sitemapName, domain,league);
-                try{
-                    let urlCache = `https://lake-api.qwiket.com/api/v50/findexar/user/get-stories?league=${league}&force=1`;
-                    await fetch(urlCache);
-                    urlCache = `https://lake-api.qwiket.com/api/v50/findexar/user/get-stories?league=&force=1`;
-                    await fetch(urlCache);
-                    urlCache=`https://lake-api.qwiket.com/api/v41/findexar/get-slug-story?slug=${slug}`;
-                    //urlCache = `https://lake-api.qwiket.com/api/v41/findexar/user/fetch-stories?league=${league}&force=1`;
+                await submitCurrentSitemap(sitemapName, domain, league);
+                try {
+                    let urlCache = `https://lake-api.qwiket.com/api/v50/findexar/get-stories?league=${league}&force=1`;
                     await fetch(urlCache);
                 }
-                catch(x){
+                catch (x) {
                     console.log("Error in fetch-stories:", x);
                 }
-
             })
-        })
-        const urlCache = `https://lake-api.qwiket.com/api/v41/findexar/user/fetch-stories?force=1`;
-        try{
+            let urlCache = `https://lake-api.qwiket.com/api/v41/findexar/get-slug-story?slug=${slug}`;
+            //urlCache = `https://lake-api.qwiket.com/api/v41/findexar/user/fetch-stories?league=${league}&force=1`;
             await fetch(urlCache);
-        }   
-        catch(x){
-            console.log("Error in fetch-stories:", x);
-        }
-
+            urlCache = `https://lake-api.qwiket.com/api/v50/findexar/fetch-stories?force=1`;
+            try {
+                await fetch(urlCache);
+            }
+            catch (x) {
+                console.log("Error in fetch-stories:", x);
+            }
+        })
         return res.status(200).json({ success: true, date })
         /**
          * Now need to get domain / newsline / forum
