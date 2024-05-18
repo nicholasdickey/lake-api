b91ab5d7a1bd657463d1fd90109de7d13ee6240e
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-05-17 20:26:33 -0400

    Add no league cache refresh.

--- pages/api/v41/findexar/sitemap/submit/[...slug].ts
+++ pages/api/v41/findexar/sitemap/submit/[...slug].ts
@@ -63,6 +63,8 @@ export default async function handler(
                 try{
                     let urlCache = `https://lake-api.qwiket.com/api/v50/findexar/user/get-stories?league=${league}&force=1`;
                     await fetch(urlCache);
+                    urlCache = `https://lake-api.qwiket.com/api/v50/findexar/user/get-stories?league=&force=1`;
+                    await fetch(urlCache);
                     urlCache=`https://lake-api.qwiket.com/api/v41/findexar/get-slug-story?slug=${slug}`;
                     //urlCache = `https://lake-api.qwiket.com/api/v41/findexar/user/fetch-stories?league=${league}&force=1`;
                     await fetch(urlCache);
